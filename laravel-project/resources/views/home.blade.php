f629e906d8166dae7bcae7dd186d48fc003485bd
Gius993 <giuseppevidili9@gmail.com>
2022-08-29 17:39:12 +0200

    stampo le località in pagina e fine esercizio

--- laravel-project/resources/views/home.blade.php
+++ laravel-project/resources/views/home.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
-</head>
-<body>
-	<h1>ciao</h1>
-</body>
-</html>
+
+@extends('layouts.app')
+
+@section('main_content')
+	<h1>Citta di viaggio</h1>
+
+	<div>
+		<ul>
+			@foreach ($trips as $trip)
+				<li>{{ $trip->locality }}</li><span>{{ $trip->description }}</span>
+			@endforeach
+		</ul>
+	</div>
+@endsection
